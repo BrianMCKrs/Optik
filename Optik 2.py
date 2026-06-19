cb5ec51d83d4b44acaccbc27493bb6b43a0ede23
Wihas Prana <wihasw@gmail.com>
2026-06-19 22:18:55 +0700

    Tambahan Optik 2.py

--- Optik 2.py
+++ Optik 2.py
@@ -23,7 +23,7 @@ if jawab.lower() == ('ya'):
         elif rumus1 == ('2'):
             print("Menghitung fokus (f), jarak benda (s), atau jarak bayangan (s')?")
             rumus2 = input()
-            if rumus2.lower == ('f'):
+            if rumus2.lower() == ('f'):
                 print("Masukkan nilai:")
                 s2 = float(input("s = "))
                 s_2= float(input("s' = "))
@@ -42,18 +42,34 @@ if jawab.lower() == ('ya'):
                 s_4= 1/(1/f4 - 1/s4)
                 print("Jarak bayangan ke cermin adalah:",round(s_4,2))
             else:
-                print("")
+                print("Anda tidak sedang menghitung!")
         else:
-            print("")
-    elif jawab2.lower == ('M+'):
-        print("Menghitung perbesaran bayangan (M), jarak bayangan (y), dan jarak benda (y')")
+            print("Anda tidak sedang menghitung!")
+    ### Menghitung Perbesaran (M)
+    elif jawab2.lower() == ('m+'):
+        print("Menghitung perbesaran bayangan (M), jarak bayangan (s') atau tinggi bayangan (y'), dan jarak benda (s) atau tinggi benda (y)")
         rumus3 = input()
-        if rumus3.lower == ('M'):
+        if rumus3.lower() == ('M'):
             print("Masukkan nilai:")
             s_5= float(input("s' = "))
             s5 = float(input("s = "))
-            m1 = -(s_5/s5)
+            m1 = abs(s_5/s5)
             print("Perbesaran bayangannya adalah:", round(m1,2))
-        elif rumus3.lower == ('y'):
-            
-           
+        elif rumus3.lower() in ['y', 's']:
+            print("Masukkan nilai:")
+            m2 = float(input("M = "))
+            y1 = float(input("y atau s = "))
+            y_1 = m2 * y1
+            print("Jarak/tinggi bayangan yang terbentuk adalah:", round(y_1,2))
+        elif rumus3.lower() in ["y'", "s'"]:
+            print("Masukkan Nilai:")
+            m3 = float(input("M = "))
+            y_2 = float(input("y' atau s' = "))
+            y2 = m3 * y_2
+            print("Jarak/tinggi benda adalah:", round(y2,2))
+        else:
+            print("Anda tidak sedang menghitung!")
+    else:
+        print("Anda tidak sedang menghitung!")
+else:
+    print("Anda tidak sedang menghitung!")
